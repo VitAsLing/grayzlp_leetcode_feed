7bd264830637fcf797a3acc9740b9d0cc33af1d9
grayzlp <grayzlp@126.com>
2017-09-11 00:51:42 +0800

    fix code comment style

--- io.grayzlp/dp/10_regular_expression_matching.py
+++ io.grayzlp/dp/10_regular_expression_matching.py
@@ -1,33 +1,68 @@
+# coding=utf-8
 """
 https://leetcode.com/problems/regular-expression-matching/description/
 
-Solution: 2D dp
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+
+The matching should cover the entire input string (not partial).
+
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
+
+Some examples:
+isMatch("aa","a") → false
+isMatch("aa","aa") → true
+isMatch("aaa","aa") → false
+isMatch("aa", "a*") → true
+isMatch("aa", ".*") → true
+isMatch("ab", ".*") → true
+isMatch("aab", "c*a*b") → true
+"""
+
+# Analysis
+"""
+Use a 2d bool array to store state whether s[0, i] (s's substring) and p[0, j] can match or not.
+When calculate the dp[i + 1][j + 1], three case:
+1. if p[j] == s[i] :   dp[i + 1][j + 1] = dp[i][j]
+2. if p[j] == '.' : dp[i + 1][j + 1] = dp[i][j]
+3. if p[j] == * :
+    1) p[j - 1] == s[i] or p[j - 1] == '.' :
+        a. dp[i][j + 1]  a* represents multiple a
+        b. dp[i + 1][j] a* represents one a
+        c. dp[i + 1][j - 1] a* represents empty
+        if a or b or c is true dp[i + 1][j + 1] can be true
+    2) otherwise 
+        dp[i + 1][j - 1] a* can only represent empty to match
 """
 
 
 class Solution(object):
     def isMatch(self, s, p):
-        dp = [[False for x in range(len(p) + 1)] for y in range(len(s) + 1)]
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        dp = [[False for i in range(len(p) + 1)] for j in range(len(s) + 1)]
         dp[0][0] = True
-        for y in range(1, len(p)):
-            if p[y] == '*' and dp[0][y - 1]:
-                dp[0][y + 1] = True
-        for x in range(0, len(s)):
-            for y in range(0, len(p)):
-                if p[y] == '.':
-                    dp[x + 1][y + 1] = dp[x][y]
-                elif s[x] == p[y]:
-                    dp[x + 1][y + 1] = dp[x][y]
-                elif p[y] == '*':
-                    if p[y - 1] != s[x] and p[y - 1] != '.':
-                        dp[x + 1][y + 1] = dp[x + 1][y - 1]
+        for j in range(1, len(p)):
+            if p[j] == '*' and dp[0][j - 1]:
+                dp[0][j + 1] = True
+        for i in range(0, len(s)):
+            for j in range(0, len(p)):
+                if p[j] == '.':
+                    dp[i + 1][j + 1] = dp[i][j]
+                elif s[i] == p[j]:
+                    dp[i + 1][j + 1] = dp[i][j]
+                elif p[j] == '*':
+                    if p[j - 1] != s[i] and p[j - 1] != '.':
+                        dp[i + 1][j + 1] = dp[i + 1][j - 1]
                     else:
-                        dp[x + 1][y + 1] = dp[x + 1][y] or dp[x][y + 1] or dp[x + 1][y - 1]
+                        dp[i + 1][j + 1] = dp[i + 1][j] or dp[i][j + 1] or dp[i + 1][j - 1]
         return dp[len(s)][len(p)]
 
 
-"""
-Test code
-"""
+# Test code
 s = Solution()
 print s.isMatch("", ".*")

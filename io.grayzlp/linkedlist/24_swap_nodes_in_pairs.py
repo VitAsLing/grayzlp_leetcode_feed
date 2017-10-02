dba58b0e59f59fc9d0dfceae5c0a8959249a681d
grayzlp <grayzlp@126.com>
2017-10-02 21:52:52 +0800

    add 24

---
+++ io.grayzlp/linkedlist/24_swap_nodes_in_pairs.py
@@ -0,0 +1,54 @@
+"""
+https://leetcode.com/problems/swap-nodes-in-pairs/description/
+
+Given a linked list, swap every two adjacent nodes and return its head.
+
+For example,
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+
+"""
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        sentinel = ListNode(0)
+        sentinel.next = head
+        pre = sentinel
+        lhs = head
+        while lhs is not None and lhs.next is not None:
+            rhs = lhs.next
+            next = rhs.next
+            pre.next = rhs
+            rhs.next = lhs
+            lhs.next = next
+            pre = lhs
+            lhs = next
+        return sentinel.next
+
+
+# Test code
+l1 = ListNode(0)
+l2 = ListNode(1)
+l3 = ListNode(2)
+l4 = ListNode(3)
+l1.next = l2
+l2.next = l3
+l3.next = l4
+ret = Solution().swapPairs(l2)
+while ret is not None:
+    print ret.val
+    ret = ret.next
+
